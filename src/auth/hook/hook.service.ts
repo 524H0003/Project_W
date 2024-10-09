7e55302dca1794aff43b0c1165819b6a08c355f1
takahashinguyen <vietuk.nguyen.git@icloud.com>
2024-10-09 15:28:31 +0700

    update hook.service.ts

--- src/auth/hook/hook.service.ts
+++ src/auth/hook/hook.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from "@nestjs/common";
-import { DatabaseRequests } from "app/utils/typeorm.utils";
-import { Hook } from "./hook.entity";
+import { Injectable } from '@nestjs/common';
+import { DatabaseRequests } from 'app/utils/typeorm.utils';
+import { Hook } from './hook.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class HookService extends DatabaseRequests<Hook> {
-	
+	constructor(@InjectRepository(Hook) repo: Repository<Hook>) {
+		super(repo);
+	}
+
+	async assign() {
+		const signature = (128).string,
+			hook = await this.save({ signature });
+
+		return { hook, signature };
+	}
 }
