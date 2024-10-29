6cdb57dc2f571a9c6f092aa7796d814a1d48a842
takahashinguyen <vietuk.nguyen.git@icloud.com>
2024-10-29 08:17:55 +0700

    update settings.json, page/src/router/index.ts and tsconfig.app.json

--- page/src/router/index.ts
+++ page/src/router/index.ts
@@ -3,7 +3,7 @@ import LoginView from '@/views/LoginView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     { path: '/login', component: LoginView },
     { path: '/:pathMatch(.*)*', component: NotFoundView },
