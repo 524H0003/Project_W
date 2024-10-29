8a2762f1f52f17c2a9a620ed297f9587dbdf47ae
takahashinguyen <vietuk.nguyen.git@icloud.com>
2024-10-29 07:03:19 +0700

    create 3 files, update 2 files and delete 4 files

---
+++ page/src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<template>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({})
+export default class LoginView extends Vue {}
+</script>
---
+++ page/src/views/NotFoundView.vue
@@ -0,0 +1,12 @@
+<template>
+	<h1>
+		<span>404</span>
+	</h1>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({})
+export default class NotFoundView extends Vue {}
+</script>
