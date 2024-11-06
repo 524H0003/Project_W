7b0dc0d5b522b4548f9dda3c86d27c6355807b8e
takahashinguyen <vietuk.nguyen.git@icloud.com>
2024-11-06 18:02:29 +0700

    update 5 files and copy 1 file

--- jest.e2e.config.ts
+++ jest.e2e.config.ts
@@ -2,17 +2,17 @@ import type { Config } from 'jest';
 
 const config: Config = {
 	testEnvironment: 'node',
+	testMatch: ['**/?(*.)+(controller.spec).ts'],
 	detectOpenHandles: true,
 	moduleDirectories: ['node_modules', 'src'],
 	transform: { '^.+.tsx?$': ['ts-jest', {}] },
-	testMatch: ['**/?(*.)+(spec).ts', '!**/?(*.)+(controller.spec).ts'],
 	reporters: [
 		'default',
 		['github-actions', { silent: false }],
-		['jest-junit', { outputDirectory: 'reports', outputName: 'junit.xml' }],
+		['jest-junit', { outputDirectory: 'reports_e2e', outputName: 'junit.xml' }],
 	],
 	collectCoverage: true,
-	coverageReporters: [['text', { file: 'coverage.txt' }]],
+	coverageReporters: [['text', { file: 'coverage_e2e.txt' }]],
 };
 
 export default config;
