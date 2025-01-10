3eba557c09809e0677ac50ef9f883d1e9e1f152d
takahashinguyen <vietuk.nguyen.git@icloud.com>
2025-01-10 09:59:18 +0700

    build(deps): update eslint.config.js, package-lock.json and package.json

--- page/eslint.config.js
+++ page/eslint.config.js
@@ -1,13 +1,11 @@
 import pluginVue from 'eslint-plugin-vue'
 import vueTsEslintConfig from '@vue/eslint-config-typescript'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
-import tsPlugin from '@typescript-eslint/eslint-plugin'
 
 export default [
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}', '**/tailwind.config.js'],
-    plugins: { tsPlugin },
+    files: ['**/*.{ts,mts,tsx,vue}'],
   },
 
   {
