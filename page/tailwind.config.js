c166a98ce1d587334b4b8effb95d72242e5950c9
takahashinguyen <vietuk.nguyen.git@icloud.com>
2025-01-10 23:40:41 +0700

    update 6 files

--- page/tailwind.config.js
+++ page/tailwind.config.js
@@ -12,6 +12,7 @@ export default {
       '&:is(.dark *)',
     ],
   ],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   plugins: [require('daisyui'), require('flowbite/plugin')],
   theme: {
     screens: {
