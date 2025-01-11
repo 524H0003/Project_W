802320f649d35bb12364d500d8d5170a468e2e86
TakahashiNguyen <TakahashiNguyen@users.noreply.github.com>
2025-01-11 01:48:48 +0000

    Apply automatic changes

--- page/vite.config.ts
+++ page/vite.config.ts
@@ -7,7 +7,5 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [basicSsl(), vue()],
   resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
-  build: {
-    outDir: '../src/app/page',
-  },
+  build: { outDir: '../src/app/page' },
 })
