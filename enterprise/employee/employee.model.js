8441538bed4057df54dc79d03546409a1f45dc39
TakahashiNguyen <TakahashiNguyen@users.noreply.github.com>
2025-01-11 07:24:32 +0000

    deploy: ee0f9a32ac8a5503c2753dd358c7640994622c13

---
+++ enterprise/employee/employee.model.js
@@ -0,0 +1,8 @@
+// Enums
+export var EmployeePosition;
+(function (EmployeePosition) {
+    EmployeePosition["Manager"] = "manager";
+    EmployeePosition["Human_resource"] = "hr";
+    EmployeePosition["Recruiter"] = "recruiter";
+    EmployeePosition["Other"] = "other";
+})(EmployeePosition || (EmployeePosition = {}));
