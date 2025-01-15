74649d403d2254f18c6d5bc87e7fc6bdab461db5
TakahashiNguyen <TakahashiNguyen@users.noreply.github.com>
2025-01-15 08:50:53 +0000

    deploy: 04ecec5cc3077caa44444a178fe55e0417a5cdc8

---
+++ notification/notification.model.js
@@ -0,0 +1,8 @@
+// Enums
+export var NotificationType;
+(function (NotificationType) {
+    NotificationType["event"] = "event";
+    NotificationType["participation"] = "participator";
+    NotificationType["progress"] = "progress";
+    NotificationType["system"] = "system";
+})(NotificationType || (NotificationType = {}));
