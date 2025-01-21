5ba5dea00abeeada1b3af09b788a178ba4460df1
takahashinguyen <vietuk.nguyen.git@icloud.com>
2025-01-21 15:09:36 +0700

    update aws.service.ts

--- src/app/aws/aws.service.ts
+++ src/app/aws/aws.service.ts
@@ -27,7 +27,7 @@ export class AWSService {
 	/**
 	 * Initiate aws service
 	 */
-	constructor(protected cfg: ConfigService) {}
+	constructor(@Inject(forwardRef(() => AppService)) private svc: AppService) {}
 
 	/**
 	 * Aws client
