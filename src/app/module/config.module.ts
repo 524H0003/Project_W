89f672a93310c29b2cde1957ea2c2928f3605f24
TakahashiNguyen <vietuk.nguyen.git@icloud.com>
2025-03-18 14:49:42 +0000

    update config.module.ts

--- src/app/module/config.module.ts
+++ src/app/module/config.module.ts
@@ -30,12 +30,7 @@ export const loadEnv =
 			SERVER_PUBLIC: Joi.string().default('./public/'),
 			SERVER_FILE_SIZE_LIMIT: Joi.number().default(256),
 			// ADMIN
-			ADMIN_EMAIL:
-				Joi.string()[
-					process.argv.some((i) => i == '--test-email')
-						? 'optional'
-						: 'required'
-				](),
+			ADMIN_EMAIL: Joi.string().default('test@test.test'),
 			// AWS
 			AWS_REGION: Joi.string().default(false),
 			AWS_ACCESS_KEY_ID: Joi.string(),
