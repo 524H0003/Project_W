64d2b9ece9908f0e6501fd05a552196f850c9581
TakahashiNguyen <vietuk.nguyen.git@icloud.com>
2025-03-26 01:18:56 +0000

    update jest.controller.config.ts, jest.resolver.config.ts and schema.gql

--- jest.resolver.config.ts
+++ jest.resolver.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
 	collectCoverage: true,
 	coverageReporters: [['text', { file: 'resolver.txt' }]],
 	collectCoverageFrom: ['src/**/*.resolver.ts'],
+	forceExit: true
 };
 
 export default config;
