889d896c9e510e7d49cf1eb06e1121d8f1539cf4
TakahashiNguyen <vietuk.nguyen.git@icloud.com>
2025-04-04 00:14:43 +0000

    update 2 files and delete 2 files

---
+++ jest.requesters.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+	testEnvironment: 'node',
+	detectOpenHandles: true,
+	moduleDirectories: ['node_modules', 'src'],
+	transform: { '^.+.tsx?$': ['ts-jest', {}] },
+	testMatch: [
+		'**/?(*.)+(controller.spec).ts',
+		'**/?(*.)+(resolver.spec).ts',
+		'src/auth/guards/**/*.spec.ts',
+	],
+	reporters: [
+		'default',
+		['github-actions', { silent: false }],
+		['jest-junit', { outputDirectory: 'reports', outputName: 'requesters.xml' }],
+	],
+	collectCoverage: true,
+	coverageReporters: [['text', { file: 'requesters.txt' }]],
+	collectCoverageFrom: [
+		'src/**/*.controller.ts',
+		'src/**/*.resolver.ts',
+		'src/auth/guards/**/*',
+	],
+	forceExit: true,
+};
+
+export default config;
