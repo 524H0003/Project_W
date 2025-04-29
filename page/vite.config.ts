fedef015ab2593c4b8bc96b5a5668fb85e166c8d
AnhNg6262 <83152264+AnhNg6262@users.noreply.github.com>
2025-04-29 17:06:05 +0000

    Apply automatic changes

--- page/vite.config.ts
+++ page/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(),],
+  plugins: [vue(), tailwindcss()],
   resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
   build: { outDir: '../src/app/page' },
 });
